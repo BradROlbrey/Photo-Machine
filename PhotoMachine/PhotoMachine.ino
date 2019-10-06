c87aebced30e1087403ab5893e33700a501741fb
BradROlbrey <berdley1@gmail.com>
2019-10-06 00:37:37 -0500

    tmc2208 cleaned up

--- PhotoMachine/PhotoMachine.ino
+++ PhotoMachine/PhotoMachine.ino
@@ -26,7 +26,7 @@ AccelStepper *stepper_arm;  // Need step_forw to be initialized first, so wait u
 const int STEP_SIZE_ARM = 16;   // 1/step_size is the actual step size, of course, but efficiency!
 const int STEPS_PER_REV_ARM = 200 * STEP_SIZE_ARM;
 const int NUM_PHOTOS_ARM = 10;  // Number of pictures to take all the way around the object.
-const double STEPS_PER_PHOTO = 5000; //STEPS_PER_REV_ARM / (double) NUM_PHOTOS_ARM;
+const double STEPS_PER_PHOTO = 10000; //STEPS_PER_REV_ARM / (double) NUM_PHOTOS_ARM;
   // (steps / rev) / (photos / rev) => (steps / photo)
   // May not always divide cleanly, want to make sure to go full 360 degree rotation!
 
@@ -44,23 +44,27 @@ void setup() {
   driver.begin();                 // SPI: Init CS pins and possible SW SPI pins
                                   // UART: Init SW UART (if selected) with default 115200 baudrate
   driver.toff(5);                 // Enables driver in software
-  driver.rms_current(600);        // Set motor RMS current (done over UART >:)
   driver.microsteps(256);          // Set microsteps to 1/16th
 
+  driver.rms_current(200);        // Set motor RMS current (done over UART >:)
+  driver.I_scale_analog(1);
+  driver.internal_Rsense(0);
   //driver.intpol(0);
+  driver.ihold(2);
+  driver.irun(31);
+  //driver.iholddelay(15);
 
-//driver.en_pwm_mode(true);       // Toggle stealthChop on TMC2130/2160/5130/5160
-//driver.en_spreadCycle(false);   // Toggle spreadCycle on TMC2208/2209/2224
+//driver.en_spreadCycle(false);   // Toggle spreadCycle on TMC2208
   driver.pwm_autoscale(true);     // Needed for stealthChop
 
 
   /*
    *  AccelStepper
-   *
+   */
   stepper_arm = new AccelStepper(step_arm, step_back_arm);
-  stepper_arm->setMaxSpeed(50);
-  stepper_arm->setAcceleration(50);
-  */
+  stepper_arm->setMaxSpeed(5000);
+  stepper_arm->setAcceleration(10000);
+  
   
   pinMode(ARM_STEP_PIN, OUTPUT);
   digitalWrite(ARM_STEP_PIN, LOW);
@@ -71,7 +75,7 @@ void setup() {
   while (!Serial);  // Wait for the Serial monitor, apparently it's s/w instead of h/w
   Serial.println("Hello, World!");  // so it takes longer to initialize.
 
-  //one_move();
+  one_move();
   
   //Serial.print("Steps per photo: ");
   //Serial.println(STEPS_PER_PHOTO);
@@ -89,27 +93,16 @@ void loop() {
     if (Serial.available()) {
       Serial.read();
       //Serial.print("Moving ");
+      
       one_move();
+      
       //Serial.println(++counter);
     }
   }
   
 }
 
-void one_move() {
-  for (uint16_t i = 0; i < 50*256; ++i) {
-    digitalWrite(ARM_STEP_PIN, HIGH);
-    digitalWrite(ARM_STEP_PIN, LOW);
-    delayMicroseconds(200);
-    //delay(1);
-    //Serial.print("Count:\t");
-    //Serial.println(i);
-  }
-  dir_arm = !dir_arm;
-  driver.shaft(dir_arm);
-}
 
-/*
 void one_move() {
   Serial.println("Moving ");
   stepper_arm->move(STEPS_PER_PHOTO);
@@ -118,12 +111,12 @@ void one_move() {
     stepper_arm->run();
   }
 }
-*/
-/*
+
+
 void step_arm() {
-  digitalWrite(PIN_STP_ARM, HIGH);
-  digitalWrite(PIN_STP_ARM, LOW);
-  
+  digitalWrite(ARM_STEP_PIN, HIGH);
+  digitalWrite(ARM_STEP_PIN, LOW);
+  /*
   curr_time = micros();
   //Serial.print(++counter);
   //Serial.print('\t');
@@ -131,5 +124,6 @@ void step_arm() {
   //Serial.print('\t');
   //Serial.println(1000000/(curr_time - prev_time));
   prev_time = micros();
-}*/
-//void step_back_arm() { /* Just reverse direction and call step_arm() */ }
+  */
+}
+void step_back_arm() { /* Just reverse direction and call step_arm() */ }
