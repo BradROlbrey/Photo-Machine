8f4d32151e7718f914e42b6eaf30bcc508bdd3e5
BradROlbrey <berdley1@gmail.com>
2019-10-06 01:30:03 -0500

    camera movement in scoot_camera function

--- PhotoMachine.py
+++ PhotoMachine.py
@@ -1,6 +1,8 @@
 
 # https://learn.sparkfun.com/tutorials/raspberry-pi-spi-and-i2c-tutorial/all#i2c-on-pi
 
+
+
 import smbus
 from time import sleep
 		
@@ -11,8 +13,6 @@ def main():
 	bus = smbus.SMBus(1)
 	arduino_addr = 0x7f
 	arduino_status = 1  # Make us wait, by default.
-	entered = False  # Used for debugging purposes.
-	
 	
 	while True:
 	
@@ -20,21 +20,23 @@ def main():
 			# ord(str) gets the ascii value of str, so we can send the
 			#	input as an int over I2C.
 	
+		scoot_camera(bus, my_move, arduino_addr)
+		print("Taking picture")
+		sleep(2)  # Take a picture
+		
+
+def scoot_camera(bus, direction, arduino_addr):
+		
 		print("Requesting move")
 		try:
-			bus.write_byte(arduino_addr, my_move)
+			bus.write_byte(arduino_addr, direction)
 		except OSError:
 			print("OSError: Failed to write to specified peripheral")
-			arduino_status = 1
 		sleep(1)
 		
-		'''
-		try:
-			arduino_status = bus.read_byte(arduino_addr)
-		except OSError:
-			print("OSError: Failed to read from specified peripheral")
-			arduino_status = 1
-		'''
+		arduino_status = 1
+		#print("Arduino_status:", arduino_status)
+		entered = False  # Keeps track of whether or not we've entered the wait loop.
 		while True:
 			try:
 				arduino_status = bus.read_byte(arduino_addr)
@@ -45,7 +47,7 @@ def main():
 				break
 			
 			print("Waiting for Arduino to finish moving", arduino_status)
-			sleep(.5)  # Poll every half second
+			sleep(.5)  # Poll every half second.
 				
 			# We have to go into here at least once. 
 			entered = True
@@ -56,11 +58,8 @@ def main():
 		else:
 			print("Dun dern still #@(%ing up >:\\\n"
 				  "Or it simply finished moving too fast because you went from 0 to 256 microsteps without changing the amount of steps it needs to take.")
-			break
-		
-		print("Taking picture")
-		sleep(2)  # Take a picture
-
+			exit(1)
+	
 
 if __name__ == '__main__':
 	main()
