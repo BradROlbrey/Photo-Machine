8768c24f69f8a4301e968ac842473fe746167ec4
BradROlbrey <berdley1@gmail.com>
2020-01-10 01:26:13 -0600

    Massive software overhaul
    There may be a hidden bug lurking somewhere that causes the slider to keep going down when it's supposed to go back up...

--- PhotoMachine.py
+++ PhotoMachine.py
@@ -7,19 +7,19 @@ import smbus
 from time import sleep
 from picamera import PiCamera
 from datetime import datetime
-		
-		
+
+
+# Initialize I2C (SMBus)
+bus = smbus.SMBus(1)
+arduino_addr = 0x7f
+
+
 def main():
 
 	camera = PiCamera()
 	camera.resolution = (3280, 2464)
 	camera.vflip = True
 	camera.start_preview()
-
-	# Initialize I2C (SMBus)
-	bus = smbus.SMBus(1)
-	arduino_addr = 0x7f
-	moving = 1  # Make us wait, by default.
 	
 	
 	'''
@@ -27,19 +27,22 @@ def main():
 	'''
 	# Disable the motors so we can manually manipulate the machine.
 	print("Disabling motors")
-	try:
-		for i in range(5):
-			bus.write_byte(arduino_addr, ord('q'))
-			sleep(.1)
+	for i in range(1):
+		write_byte(ord('q'), "OSError: Failed to disable motor drivers", 1)
 			# For some reason, sending first 'q' is unreliable. Delay does not fix,
 			#	so sending multiple q's with small delays.
 			# Oddly enough, the Arduino's receive function recognizes the 'q', but
 			#	then it never goes into the switch statement.
 			# Sending 'q' or other letters doesn't appear to be the slightest problem
 			#	later on, thankfully.
-	except OSError:
-		print("OSError: Failed to disable motor drivers")
-		exit(1)
+	
+	# try:
+	# 	for i in range(5):
+	# 		bus.write_byte(arduino_addr, ord('q'))
+	# 		sleep(.1)
+	# except OSError:
+	#	print("OSError: Failed to disable motor drivers")
+	#	exit(1)
 	
 	print("Make sure the Arm is rotated such that the motor wires twist properly and Slider is at bottom-most position")
 	
@@ -68,7 +71,7 @@ def main():
 	print("Sending input to arduino")
 	try:
 		bus.write_i2c_block_data(arduino_addr, 0, [num_photos_per_rev, num_levels])
-		sleep(.1)  # Give some time for the Arduino to process back-to-back I2C comms.
+		#sleep(.1)  # Give some time for the Arduino to process back-to-back I2C comms.
 	except OSError:
 		print("OSError: Failed to send num_levels.")
 		exit(1)
@@ -79,12 +82,15 @@ def main():
 	'''
 	# Re-enable stepper drivers when this is done.
 	print("Enabling motors")
-	try:
-		bus.write_byte(arduino_addr, ord('e'))  # 'e' for enable motors!
-		sleep(.1)
-	except OSError:
-		print("OSError: Failed to enable motor drivers")
-		exit(1)
+	write_byte(ord('e'), "OSError: Failed to enable motor drivers", 1)
+
+	# try:
+	# 	wait_for_processing()
+	# 	bus.write_byte(arduino_addr, ord('e'))  # 'e' for enable motors!
+	# 	#sleep(.1)
+	# except OSError:
+	# 	print()
+	# 	exit(1)
 	
 	
 	'''
@@ -109,17 +115,18 @@ def main():
 			total_pics = i * num_levels + j + 1
 			name = 'test-{}_{:0>2}-{:0>2}_{:0>3}.jpg'.format(time, i+1, j+1, total_pics)
 
-			wait_for_moving(bus, arduino_addr)
-			sleep(.5)
+			#print("Waiting for move in loops")
+			wait_for_moving()
+			sleep(.25)
 			print("Taking picture", name)
 			camera.capture(name)
-			sleep(.5)
+			sleep(.25)
 			
 			# Scoot up as we take photos
 			# ord(str) gets the ascii value of str, so we can send the
 			#	input as an int over I2C.
 			if j != num_levels-1:
-				scoot_camera(bus, ord('w'), arduino_addr)
+				write_byte(ord('w'))
 			# else continue
 			# Need the, say, 5 photos. But only want 4 moves. So skip move on last loop.
 
@@ -127,57 +134,79 @@ def main():
 		# Need the, say, 3 levels. But only want 2 move rotations. So skip rotation on last loop.
 		# Will handle last move down outside of loop.
 		if i != num_photos_per_rev-1:
-			scoot_camera(bus, ord('s'), arduino_addr)
-			wait_for_processing(bus, arduino_addr)
-			scoot_camera(bus, ord('d'), arduino_addr)
+			write_byte(ord('s'))
+			write_byte(ord('d'))
 	
 	# Rotate the camera back to the beginning and return down all the way.
-	scoot_camera(bus, ord('s'), arduino_addr)
-	wait_for_processing(bus, arduino_addr)
-	scoot_camera(bus, ord('a'), arduino_addr)
-	wait_for_moving(bus, arduino_addr)
+	write_byte(ord('s'))
+	write_byte(ord('a'))
+	wait_for_moving()
 
 	print("Finished! Disabling motor drivers")
-	try:
-		bus.write_byte(arduino_addr, ord('q'))
-	except OSError:
-		print("OSError: Failed to disable motor drivers")
+	write_byte(ord('q'), "OSError: Failed to disable motor drivers")
+
+	# try:
+	# 	bus.write_byte(arduino_addr, ord('q'))
+	# except OSError:
+	# 	print("OSError: Failed to disable motor drivers")
 		
 
-def scoot_camera(bus, direction, arduino_addr):
-	#print("Requesting move")
+def write_byte(inp_byte, custom_message="OSError: Failed to write byte to specified peripheral", exit_status=0):
+	# Everytime we write a byte, we must
+	# 	do error handling and
+	# 	wait for arduino to be ready to accept more input.
+	
+	wait_for_processing()
+
 	try:
-		bus.write_byte(arduino_addr, direction)
+		print("Writing byte:", inp_byte)
+		bus.write_byte(arduino_addr, inp_byte)
 	except OSError:
-		print("OSError: Failed to write to specified peripheral")
-	sleep(.1)  # Give the Arduino some tiny breathing room to receive the input and
-	# set its status (int moving) before requesting it.
-	# Doesn't matter if it finished moving before this is done. Probably.
+		print(custom_message)
+
+		if exit_status:
+			exit(exit_status)
+
+
+# def scoot_camera(direction):
+# 	#print("Requesting move")
+# 	try:
+# 		bus.write_byte(arduino_addr, direction)
+# 	except OSError:
+# 		print("OSError: Failed to write to specified peripheral")
+# 	sleep(.1)  # Give the Arduino some tiny breathing room to receive the input and
+# 	# set its status (int moving) before requesting it.
+# 	# Doesn't matter if it finished moving before this is done. Probably.
 
 	
-def wait_for_moving(bus, arduino_addr):
+def wait_for_moving():
 	while True:
-		moving = read_bytes_from_arduino(bus, arduino_addr)[0]
-		if not moving:
+		print("Waiting for move in function:", end=' ')
+		moving = read_bytes_from_arduino(0)[0]
+		print("Moving:", moving)
+		if moving == 0:
 			return
 		#print("Waiting for Arduino to finish moving", arduino_status)
 		sleep(.5)
 		
-def wait_for_processing(bus, arduino_addr):
+def wait_for_processing():
 	while True:
-		processing = read_bytes_from_arduino(bus, arduino_addr)[1]
-		if not processing:
+		processing = read_bytes_from_arduino(1)[1]
+		print("Processing:", processing)
+		if processing == 0:
 			return
 		sleep(.1)
 
-def read_bytes_from_arduino(bus, arduino_addr):
+def read_bytes_from_arduino(which_byte):
 	buffer = None
-	while True:
+	for i in range(10):  # Try 10 times, if it doesn't work, exit.
 		try:
-			buffer = bus.read_i2c_block_data(arduino_addr, 0, 2)
+			buffer = bus.read_i2c_block_data(arduino_addr, which_byte, 2)
 			return buffer
 		except OSError:
 			print("OSError: Failed to read from specified peripheral")
+			sleep(.5)
+	print("Failed 10 times trying to read from Arduino, something wrong, exiting program.")
 
 
 if __name__ == '__main__':
--- PhotoMachine/PhotoMachine.ino
+++ PhotoMachine/PhotoMachine.ino
@@ -20,6 +20,7 @@ TMC2208Stepper driver(SW_RX, SW_TX, R_SENSE);
 // Don't think these need to be volatile because they're read-only.
 const long TOTAL_STEPS_AROUND = 200L * MICROSTEPS;
 const long TOTAL_STEPS_UP = 300L * MICROSTEPS;
+//const long TOTAL_STEPS_UP = 50L * MICROSTEPS;
 
 /*
  *  AccelStepper
@@ -29,11 +30,11 @@ AccelStepper *stepper_line;
 
 #define ENA_PIN             7  // For both motors
 
-#define ARM_DIR_PIN         5   // Direction
-#define ARM_STEP_PIN        6   // Step
+#define ARM_DIR_PIN         8   // Direction
+#define ARM_STEP_PIN        9   // Step
 
-#define LINE_DIR_PIN        8
-#define LINE_STEP_PIN       9
+#define LINE_DIR_PIN        5
+#define LINE_STEP_PIN       6
 
 
 /*
@@ -55,7 +56,10 @@ bool line_dir = false;
 volatile char next_byte = ' ';  // Stores the value from Wire.read().
 volatile byte moving = 0;       // Tells the Pi whether the camera is still moving. Pi will wait
                                 // until camera stops before taking a picture.
-volatile byte processing = 0;   // Tells the Pi when the Arduino is 
+bool executed_moving = false;
+volatile byte processing = 0;   // Tells the Pi when the Arduino is ready to receive
+                                // more instructions (bytes).
+bool executed_processing = false;
 
 /*
  *  Serial.printing
@@ -80,7 +84,7 @@ void setup() {
   driver.I_scale_analog(1);
   driver.internal_Rsense(0);
   //driver.intpol(0);
-  driver.ihold(2);
+  driver.ihold(1);
   driver.irun(31);
   driver.iholddelay(15);
 
@@ -94,7 +98,7 @@ void setup() {
   pinMode(ENA_PIN, OUTPUT);
   digitalWrite(ENA_PIN, HIGH);  // HIGH is disabled
   
-  stepper_arm = new AccelStepper(step_arm, step_back_arm);
+  stepper_arm = new AccelStepper(step_arm, step_arm);  // Yes, they are the same
   stepper_arm->setMaxSpeed(50L * MICROSTEPS);
   stepper_arm->setAcceleration(50L * MICROSTEPS);
   
@@ -103,7 +107,7 @@ void setup() {
   pinMode(ARM_DIR_PIN, OUTPUT);
   digitalWrite(ARM_DIR_PIN, LOW);
   
-  stepper_line = new AccelStepper(step_line, step_back_line);
+  stepper_line = new AccelStepper(step_line, step_line);
   stepper_line->setMaxSpeed(100L * MICROSTEPS);
   stepper_line->setAcceleration(100L * MICROSTEPS);
   
@@ -123,126 +127,177 @@ void setup() {
   /*
    *  Serial
    */
-  //while (!Serial);  // Wait for the Serial monitor, apparently it's s/w instead of h/w
-  //Serial.println("Hello, World!");  // so it takes longer to initialize.
+  while (!Serial);  // Wait for the Serial monitor, apparently it's s/w instead of h/w
+  Serial.println("Hello, World!");  // so it takes longer to initialize.
 
   /*
    *  Photo stuff
    */
   
-  print_interval = 5000;
+  /*print_interval = 5000;
   prev_time = millis() - print_interval;
   // Wait to receive over I2C
-  while (num_photos_per_rev == NULL ||
-         num_levels == NULL) {
+  while (steps_per_photo_around == NULL ||
+         steps_per_level == NULL) {
     if (millis() - prev_time > print_interval) {
       Serial.println("Waiting to receive num_photos and num_levels");
       prev_time += print_interval;
     }
-  }
+  }*/
   print_interval = 250;
-  Serial.println();
-  
-  Serial.print("Pictures per rotation: ");
-  Serial.println(num_photos_per_rev);
-  Serial.print("Number of levels: ");
-  Serial.println(num_levels);
-  Serial.print("Total number of pictures to take: ");
-  Serial.println(num_photos_per_rev * num_levels);
-
   Serial.println();
 }
 
+
 void loop() {
+  //Serial.println("\nRunning");
   stepper_arm->run();
   stepper_line->run();
+  
 
-  if (stepper_arm->distanceToGo() == 0 &&
-      stepper_line->distanceToGo() == 0)
+  if (stepper_arm->distanceToGo() == 0
+      && stepper_line->distanceToGo() == 0
+      && executed_moving == true  // ensure that we've run through a case before reseting moving to false
+    )
     {
+    Serial.println("Moving set to 0");
     moving = 0;
+    executed_moving = false;
   }
+  
 
-  bool executed = false;
+  /*Serial.print("before switch '");
+  Serial.print(next_byte);
+  Serial.print("' ");
+  Serial.print(processing);
+  Serial.print(' ');
+  Serial.println(executed);*/
   switch (next_byte) {
     case ' ':
-      executed = true;
+      //executed_processing = true;  // This one doesn't count!
+      //Serial.println("Blank");
       break;
       
     case 'w':  // Slide camera up
-      executed = true;  // As soon as we've entered switch statement,
+      executed_processing = true;
+      executed_moving = true;
       Serial.println('w');
       digitalWrite(LINE_DIR_PIN, HIGH);
       stepper_line->move(steps_per_level);
       break;
       
     case 'd':  // Rotate arm clockwise
-      executed = true;  // set true so Pi can immediately send more data.
+      executed_processing = true;
+      executed_moving = true;
       Serial.println('d');
       digitalWrite(ARM_DIR_PIN, LOW);
       stepper_arm->move(steps_per_photo_around);
       break;
       
     case 's':  // Slide camera down to bottom
+      /*Serial.print("Curr stepper_line position: ");
+      Serial.println(stepper_line->currentPosition());
+      Serial.print("Targ stepper_line position: ");
+      Serial.println(stepper_line->targetPosition());*/
+      
+      executed_processing = true;
+      executed_moving = true;
       Serial.println('s');
       digitalWrite(LINE_DIR_PIN, LOW);
       stepper_line->moveTo(0);  // absolute
-      executed = true;
+      
+      /*Serial.print("Curr stepper_line position: ");
+      Serial.println(stepper_line->currentPosition());
+      Serial.print("Targ stepper_line position: ");
+      Serial.println(stepper_line->targetPosition());*/
+      
       break;
       
     case 'a':  // Rotate arm all the way counter-clockwise (returns to start position)
+      executed_processing = true;
+      executed_moving = true;
       Serial.println('a');
       digitalWrite(ARM_DIR_PIN, HIGH);
       stepper_arm->moveTo(0);  // absolute
-      executed = true;
       break;
 
     case 'e':  // Enable motors
+      executed_processing = true;
       Serial.println('e');
       digitalWrite(ENA_PIN, LOW);
-      executed = true;
       break;
       
     case 'q':  // Disable motors
+      executed_processing = true;
       Serial.println('q');
       digitalWrite(ENA_PIN, HIGH);
-      executed = true;
       break;
       
     default:
-      Serial.print("Invalid input");
-      executed = true;
+      executed_processing = true;
+      Serial.println("Invalid input");
   }
-  if (executed) {
+  
+  /*Serial.print("after switch '");
+  Serial.print(next_byte);
+  Serial.print("' ");
+  Serial.print(processing);
+  Serial.print(' ');
+  Serial.println(executed);*/
+    
+  if (executed_processing) {
+    delay(1000);  // To force check Pi's wait_for_processing
+    // In here so that outer loop can run and Arduino can "finish" moving.
+    Serial.println("executed_processing ");
     next_byte = ' ';
+    //Serial.print(next_byte);
+    //Serial.print("' ");
     processing = 0;  // We have processed the Pi's input.
-
+    //Serial.println(processing);
+    executed_processing = false;
   }
-  // Don't need to reset executed because it is initialized to false each loop.
   // Receiving bytes needs to be blocking in case the Pi sends two bytes in quick
   // succession before we get around to processing the first. This could happen anywhere!
 }
 
 
 // Receive data from Pi
+// Interrupts delay(), but Arduino will continue waiting after returning from receiveEvent.
 void receiveEvent(int howMany) {
   if (howMany == 1) {
-    // Receiving the usual wasd or qe
-    next_byte = Wire.read();
+    byte recvd_byte = Wire.read();
     Serial.print("Rec: ");
-    Serial.println(next_byte);
-
-    processing = 1;
-    moving = 1;
+    Serial.println(int(recvd_byte));
+    
+    // Two cases:
+    //  1. Receiving a "register", as the Pi smbus docs call it, which we'll ignore.
+    //  2. Receiving the usual wasd or qe, which we'll process.
+    if (recvd_byte != 0 && recvd_byte != 1) {
+      processing = 3;
+      
+      next_byte = recvd_byte;
+      Serial.print("Set ");
+      Serial.println(next_byte);
+      
+      if (next_byte != 'e' && next_byte != 'q')  // 'e' and 'q' don't constitute moving
+        moving = 2;
+    }
   }
-  else {
-    // Receiving two important setup integers (actually they're bytes, so we can't do more than
-    //  255? or 127? around or about, but that can be expanded some other time if necessary,
-    //  perhaps by bitshifting multiple bytes in).
+  else /* howMany == 3 */ {
+    // Receiving two important setup integers (actually they're bytes, 0-255).
     int dont_care = Wire.read();  // Could be useful in the future as a code or something.
+    
     // Don't think these need to be volatile b/c they're inside the interrupt.
     int num_photos_per_rev = Wire.read();  // Number of pictures to take all the way around the object.
     int num_levels = Wire.read();  // Number of rotations to make, number of times camera moves up.
+
+    // Probably shouldn't have print statements in an interrupt...
+    Serial.print("Pictures per rotation: ");
+    Serial.println(num_photos_per_rev);
+    Serial.print("Number of levels: ");
+    Serial.println(num_levels);
+    Serial.print("Total number of pictures to take: ");
+    Serial.println(num_photos_per_rev * num_levels);
   
     steps_per_photo_around = TOTAL_STEPS_AROUND / num_photos_per_rev;
     steps_per_level = TOTAL_STEPS_UP / (num_levels - 1);
@@ -251,8 +306,12 @@ void receiveEvent(int howMany) {
 // Send data to Pi
 void requestEvent() {
   delayMicroseconds(8);
-  byte temp = [moving, processing];
-  Wire.write(temp);
+  Serial.print("Sending requestEvent()\t");
+  Serial.print(moving);
+  Serial.print("\t");
+  Serial.println(processing);
+  byte temp[] = {moving, processing};
+  Wire.write(temp, 2);
 }
 
 
